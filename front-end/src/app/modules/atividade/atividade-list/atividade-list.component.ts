7d081d51489cc5c2cb76652d8c7f73412e04e7e0
Daniel Alves <danielbsb06@gmail.com>
2020-08-24 13:18:26 -0300

    feat(front): cria componente lista atividade

--- front-end/src/app/modules/atividade/atividade-list/atividade-list.component.ts
+++ front-end/src/app/modules/atividade/atividade-list/atividade-list.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 
 @Component({
   selector: 'app-atividade-list',
-  templateUrl: './atividade-list.component.html'
+  templateUrl: './atividade-list.component.html',
+  styleUrls:['./atividade-list.component.css']
 })
 export class AtividadeListComponent implements OnInit {
+
+  private listaAtividades = [];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  excluir(atividade){
+    this.listaAtividades.splice(this.listaAtividades.indexOf(atividade),1);
+  }
 }
--- front-end/src/app/shared/shared.module.ts
+++ front-end/src/app/shared/shared.module.ts
@@ -1,15 +1,23 @@
 import {ModuleWithProviders, NgModule} from '@angular/core';
-import {ReactiveFormsModule} from '@angular/forms';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
-
 import {LOCALE_ID} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {InputTextModule} from 'primeng/inputtext';
+import {FieldsetModule} from 'primeng/fieldset';
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
 
 @NgModule({
     imports: [
         BrowserModule, 
         CommonModule,
-        ReactiveFormsModule,    
+        ReactiveFormsModule,  
+        FormsModule,  
+        InputTextModule,
+        FieldsetModule,
+        ButtonModule,
+        TableModule
     ],
     declarations: [
     ],
@@ -17,9 +25,14 @@ import { BrowserModule } from '@angular/platform-browser';
         {provide: LOCALE_ID,useValue: "pt-BR"}
     ],
     exports: [
-        BrowserModule,
+        BrowserModule, 
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,  
+        FormsModule,  
+        InputTextModule,
+        FieldsetModule,
+        ButtonModule,
+        TableModule
     ]
 })
 export class SharedModule {

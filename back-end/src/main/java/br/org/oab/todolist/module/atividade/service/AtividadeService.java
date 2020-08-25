9efe3b3758a55756869856e7591c7ec8b865bc18
Daniel Alves <danielbsb06@gmail.com>
2020-08-24 21:25:37 -0300

    perf: habilitado cache lista atividades

--- back-end/src/main/java/br/org/oab/todolist/module/atividade/service/AtividadeService.java
+++ back-end/src/main/java/br/org/oab/todolist/module/atividade/service/AtividadeService.java
@@ -5,6 +5,8 @@ import br.org.oab.todolist.module.atividade.entity.Atividade;
 import br.org.oab.todolist.module.atividade.mapper.AtividadeMapper;
 import br.org.oab.todolist.module.atividade.repository.AtividadeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,10 +20,12 @@ public class AtividadeService {
     @Autowired
     private AtividadeMapper atividadeMapper;
 
+    @Cacheable("atividade")
     public List<AtividadeDTO> listarTodas() {
         return atividadeMapper.toDto((List<Atividade>) atividadeRepository.findAll());
     }
 
+    @CacheEvict(value="atividade",  allEntries = true)
     public AtividadeDTO salvar(AtividadeDTO atividadeDTO) {
         return atividadeMapper.toDto(atividadeRepository.save(atividadeMapper.toEntity(atividadeDTO)));
     }
